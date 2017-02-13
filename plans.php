29ff16547bd7fa137206966c98958432996fbfa5
MAHADADA <MAHADADA@users.noreply.github.com>
2017-02-13 07:18:13 -0600

    merge

--- plans.php
+++ plans.php
@@ -3,13 +3,20 @@
 require('config/config.php');
 require('func/plantype.php');
 require('func/tag.php');
-$admin = false;
-if (isset($_GET['admin'])) {
-	$admin = true;
+$admin = isset($_GET['admin']);
+if ($admin) {
+	$sthyear = $G["db"]->prepare("SELECT MIN(`year`) AS 'minyear',MAX(`year`) AS 'maxyear' FROM `plan`");
+	$sth = $G["db"]->prepare("SELECT * FROM `plan` ORDER BY `year` DESC, `type` ASC, `name` ASC");
+} else {
+	$sthyear = $G["db"]->prepare("SELECT MIN(`year`) AS 'minyear',MAX(`year`) AS 'maxyear' FROM `plan` WHERE `inuse` = 1");
+	$sth = $G["db"]->prepare("SELECT * FROM `plan` WHERE `inuse` = 1 ORDER BY `year` DESC, `type` ASC, `name` ASC");
 }
-$sth = $G["db"]->prepare('SELECT * FROM `plan` ORDER BY `year` DESC, `type` ASC, `name` ASC');
+$sthyear->execute();
+$row = $sthyear->fetch(PDO::FETCH_ASSOC);
+$minyear = $row["minyear"];
+$maxyear = $row["maxyear"];
 $sth->execute();
-$planlist=$sth->fetchAll(PDO::FETCH_ASSOC);
+$planlist = $sth->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html lang="zh-Hant-TW">
 <head>
@@ -35,10 +42,22 @@ function alltag(){
 	for (var i = 0; i < filter_tag.length; i++) {
 		filter_tag[i].checked = true;
 	}
+	filter_notag.checked = true;
 }
 function notag(){
 	for (var i = 0; i < filter_tag.length; i++) {
 		filter_tag[i].checked = false;
+	}
+	filter_notag.checked = false;
+}
+function changeandor(){
+	if (tagand.checked) {
+		filter_notag.checked = false;
+		filter_notag.disabled = true;
+		filter_notag.classList.add("disabled");
+	} else {
+		filter_notag.disabled = false;
+		filter_notag.classList.remove("disabled");
 	}
 }
 window.onload=function(){
@@ -85,13 +104,13 @@ function filter(){
 }
 </script>
 <div class="container">
-	<h2>教案<?=($admin?"管理":"查詢")?><?php if($admin){ ?> <a class="btn btn-md btn-primary" href="<?=$C["path"]?>/newplan/" role="button">新增</a><?php }?></h2>
+	<h2>教案<?=($admin?"管理":"查詢")?><?php if($admin){ ?> <a class="btn btn-sm btn-primary" href="<?=$C["path"]?>/newplan/" role="button">新增</a><?php }?></h2>
 	<div class="row">
 		<label class="col-sm-2 form-control-label">學年度</label>
 		<div class="col-sm-10 form-inline">
-			<input type="number" class="form-control" placeholder="起始" id="filter_year1" oninput="filter()" style="max-width: 45%;">
+			<input type="number" class="form-control" placeholder="起始" id="filter_year1" value="<?=$minyear?>" oninput="filter()" style="max-width: 45%;">
 			<span class="form-control-static">至</span>
-			<input type="number" class="form-control" placeholder="結束" id="filter_year2" oninput="filter()" style="max-width: 45%;">
+			<input type="number" class="form-control" placeholder="結束" id="filter_year2" value="<?=$maxyear?>" oninput="filter()" style="max-width: 45%;">
 		</div>
 	</div>
 	<div class="row">
@@ -121,19 +140,22 @@ function filter(){
 		<label class="col-sm-2 form-control-label">標籤</label>
 		<div class="col-sm-10">
 			<div class="checkbox">
-				<label class="checkbox-inline" onchange="filter()">
+				<label class="checkbox-inline" onchange="changeandor();filter();" data-toggle="tooltip" data-placement="bottom" title="同時包含這些標籤">
 					<input type="radio" name="tagandor" id="tagand" value="and">AND
 				</label>
-				<label class="checkbox-inline" onchange="filter()">
+				<label class="checkbox-inline" onchange="changeandor();filter();" data-toggle="tooltip" data-placement="bottom" title="包含任一標籤">
 					<input type="radio" name="tagandor" id="tagor" value="or" checked>OR
 				</label>
 				<?php
 				foreach ($D['tag'] as $tag => $cnt) {
 					?><label class="checkbox-inline" onchange="filter()">
-						<input type="checkbox" id="filter_tag" value="<?=$tag?>" checked><?=$tag?>
+						<input type="checkbox" id="filter_tag" value="<?=$tag?>" checked><mark><?=$tag?></mark>
 					</label> <?php
 				}
 				?>
+				<label class="checkbox-inline" onchange="filter()" data-toggle="tooltip" data-placement="bottom" title="僅在OR模式作用">
+					<input type="checkbox" id="filter_notag" checked>無標籤
+				</label>
 				<button type="button" class="btn btn-primary btn-sm" onclick="alltag();filter();">全選</button> 
 				<button type="button" class="btn btn-primary btn-sm" onclick="notag();filter();">全不選</button>
 			</div>
@@ -146,7 +168,7 @@ function filter(){
 				<th>分類</th>
 				<th>標題</th>
 				<th>標籤</th>
-				<th><?=($admin?"管理":"詳情")?></th>
+				<th>動作</th>
 			</thead>
 			<tbody id="plantable">
 			<?php
@@ -163,14 +185,11 @@ function filter(){
 					}
 				?></td>
 				<td>
+				<a class="btn btn-sm btn-primary" href="<?=$C["path"]?>/plan/<?=$plan['id']?>/" role="button">查看</a>
 				<?php
 				if ($admin) {
 				?>
-				<a class="btn btn-sm btn-primary" href="<?=$C["path"]?>/manage/plan/edit/?id=<?=$plan['id']?>" role="button">管理</a>
-				<?php
-				} else {
-				?>
-				<a class="btn btn-sm btn-primary" href="<?=$C["path"]?>/view/plan/?id=<?=$plan['id']?>" role="button">詳情</a>
+				<a class="btn btn-sm btn-primary" href="<?=$C["path"]?>/editplan/<?=$plan['id']?>/" role="button">編輯</a>
 				<?php
 				}
 				?>
@@ -190,5 +209,10 @@ require("footer.php");
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+<script type="text/javascript">
+$(function () {
+	$('[data-toggle="tooltip"]').tooltip()
+})
+</script>
 </body>
 </html>
