ec64fd06da340f351ca91c011c3724cee7e251c8
xiplus <huangxuanyuxiplus@gmail.com>
2017-02-15 20:56:04 +0800

    filename filter control char

--- config/config.smaple.php
+++ config/config.smaple.php
@@ -14,8 +14,8 @@ $C["FilenamePattern"] = '[^';
 foreach (str_split($C["FilenameReserved"]) as $char) {
 	$C["FilenamePattern"] .= '\x'.sprintf("%x", ord($char));
 }
-$C["FilenamePattern"] .= ']+';
-$C["FilenameTitle"] = "不可包含以下字元: ".htmlentities(implode(" ", str_split($C["FilenameReserved"])));
+$C["FilenamePattern"] .= '\x00-\x1f\x7f]+';
+$C["FilenameTitle"] = "不可包含控制字元和以下字元: ".htmlentities(implode(" ", str_split($C["FilenameReserved"])));
 
 $G["db"] = new PDO ('mysql:host='.$C["DBhost"].';dbname='.$C["DBname"].';charset=utf8', $C["DBuser"], $C["DBpass"]);
 $G["schoolyear"] = date("Y")-1911-(date("m")<=8);
